2676427b43e8d6753141f5c548e4555ab05ebf9f
Katya Fadeeva <katfadmail@gmail.com>
2016-02-21 06:43:58 +0200

    base js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function shuffle(array){
+    return array;
+}
+
+var orderedBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var shuffleBoard = shuffle(orderedBoard);
+//console.log(shuffleBoard);
