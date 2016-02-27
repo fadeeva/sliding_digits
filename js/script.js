e9953622a6b71ea07a5ccfef6478413cf47427a7
Katya Fadeeva <katfadmail@gmail.com>
2016-02-28 00:43:43 +0200

    Grid

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-function shuffle(array){
-    return array;
-}
+// Grid(top, left):
+/*
+|    (0, 0)    (0, 137)    (0, 240)    (0, 343)
+|   (107, 0)  (107, 137)  (107, 240)  (107, 343)
+|   (214, 0)  (214, 137)  (214, 240)  (214, 343)
+|   (321, 0)  (321, 137)  (321, 240)  (321, 343)
+*/
 
-var orderedBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-var shuffleBoard = shuffle(orderedBoard);
-//console.log(shuffleBoard);
+$(document).ready(function(){
+    function shuffle(array){
+        return array;
+    }
+
+    var orderedBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    var shuffleBoard = shuffle(orderedBoard);
+    //console.log(shuffleBoard);
+});
