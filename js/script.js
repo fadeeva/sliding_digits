9ff46fbda0d88b9600ccbdbebd031207ce8765af
Katya Fadeeva <katfadmail@gmail.com>
2016-03-01 18:41:48 +0200

    problem with accumulated pixels is fixed

--- js/script.js
+++ js/script.js
@@ -7,17 +7,10 @@
 */
 
 $(document).ready(function(){
-    function shuffle(array){
-        return array;
-    }
 
-    var orderedBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    var shuffleBoard = shuffle(orderedBoard);
-    //console.log(shuffleBoard);
-    
     $(".button").click(function(){   
-        var digitButton = $(this) // получить id
-        var nullButton = $(".null") // получить id
+        var digitButton = $(this);
+        var nullButton = $(".null");
         var digitButtonId = digitButton.get(0).id;
         var nullButtonId = nullButton.get(0).id;
         
@@ -26,23 +19,110 @@ $(document).ready(function(){
         var xNull = nullButton.get(0).id.slice(1, 2);
         var yNull = nullButton.get(0).id.slice(4, 5);
         
-       
+        var nullButtonCoordanates = getCoordinates(nullButtonId);
+        var digitButtonCoordinates = getCoordinates(digitButtonId);
         
         if( ((x == xNull) && (Math.abs(y - yNull) == 1)) || ((y == yNull) && (Math.abs(x - xNull) == 1))) {
-            digitButton.animate({"top": nullButton.position().top, 
-                                 "left": nullButton.position().left});
-            nullButton.animate({"top": digitButton.position().top, 
-                                 "left": digitButton.position().left});
+            digitButton.animate({"top": nullButtonCoordanates.top,
+                                 "left": nullButtonCoordanates.left});
+            nullButton.animate({"top": digitButtonCoordinates.top, 
+                                "left": digitButtonCoordinates.left});
             
             digitButton.removeAttr("id");
             digitButton.attr("id", nullButtonId);
             nullButton.removeAttr("id");
             nullButton.attr("id", digitButtonId);
-            
-            
-        }
-        
+        }  
         
     })
     
 });
+
+function getCoordinates(elementID) {
+    var coordinates = {
+        top: 0,
+        left: 0
+    };
+    
+    switch(elementID) {
+        /*-ROW1-------------------------------*/
+        case "x1_y1" : 
+                        coordinates.top = 0;
+                        coordinates.left = 34;
+                        break;
+        case "x1_y2" : 
+                        coordinates.top = 0;
+                        coordinates.left = 137;
+                        break;
+        case "x1_y3" : 
+                        coordinates.top = 0;
+                        coordinates.left = 240;
+                        break;
+        case "x1_y4" : 
+                        coordinates.top = 0;
+                        coordinates.left = 343;
+                        break;
+        /*-ROW2-------------------------------*/
+        case "x2_y1" : 
+                        coordinates.top = 107;
+                        coordinates.left = 34;
+                        break;
+        case "x2_y2" : 
+                        coordinates.top = 107;
+                        coordinates.left = 137;
+                        break;
+        case "x2_y3" : 
+                        coordinates.top = 107;
+                        coordinates.left = 240;
+                        break;
+        case "x2_y4" : 
+                        coordinates.top = 107;
+                        coordinates.left = 343;
+                        break;
+        /*-ROW3-------------------------------*/
+        case "x3_y1" : 
+                        coordinates.top = 214;
+                        coordinates.left = 34;
+                        break;
+        case "x3_y2" : 
+                        coordinates.top = 214;
+                        coordinates.left = 137;
+                        break;
+        case "x3_y3" : 
+                        coordinates.top = 214;
+                        coordinates.left = 240;
+                        break;
+        case "x3_y4" : 
+                        coordinates.top = 214;
+                        coordinates.left = 343;
+                        break;
+        /*-ROW4-------------------------------*/
+        case "x4_y1" : 
+                        coordinates.top = 321;
+                        coordinates.left = 34;
+                        break;
+        case "x4_y2" : 
+                        coordinates.top = 321;
+                        coordinates.left = 137;
+                        break;
+        case "x4_y3" : 
+                        coordinates.top = 321;
+                        coordinates.left = 240;
+                        break;
+        case "x4_y4" : 
+                        coordinates.top = 321;
+                        coordinates.left = 343;
+                        break;
+        
+        default : break;
+    }
+    return coordinates;
+}
+
+function shuffle(array){
+    return array;
+}
+
+var orderedBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var shuffleBoard = shuffle(orderedBoard);
+//console.log(shuffleBoard);
