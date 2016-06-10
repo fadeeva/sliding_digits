cf8c7e1b4437f540d04a026e63fac8ba4fd4ffb4
Katya Fadeeva <katfadmail@gmail.com>
2016-06-10 23:51:19 +0200

    the watch

--- js/watch.js
+++ js/watch.js
@@ -14,10 +14,11 @@ function timedCount(){
     document.getElementById('stopwatch').innerHTML = c;
     c++;
     if(c - 10 > 0)
-        document.getElementById('stopwatch').style.marginLeft = "100px";
+        document.getElementById('stopwatch').style.marginLeft = "87px";
     if(c - 100 > 0)
-        document.getElementById('stopwatch').style.marginLeft = "90px";
-    
+        document.getElementById('stopwatch').style.marginLeft = "27px";
+    if(c == 999)
+        stopWatch();
 }
 
 function startWatch(){
@@ -30,6 +31,6 @@ function startWatch(){
 
 function restartWatch(){
     c = 0;
-    document.getElementById('stopwatch').style.marginLeft = "116px";
+    document.getElementById('stopwatch').style.marginLeft = "147px";
     document.getElementById('stopwatch').innerHTML = c;
 }
